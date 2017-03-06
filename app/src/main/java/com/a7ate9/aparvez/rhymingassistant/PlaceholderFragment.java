752c89110e6195c6d23cffa0d06d1b0812c67acf
Parvez Hassan Ansari <aparvez@thoughtworks.com>
2017-03-06 22:43:23 +0530

    add rhyming words to an array parsed from JSON from api

--- app/src/main/java/com/a7ate9/aparvez/rhymingassistant/PlaceholderFragment.java
+++ app/src/main/java/com/a7ate9/aparvez/rhymingassistant/PlaceholderFragment.java
@@ -1,6 +1,8 @@
 package com.a7ate9.aparvez.rhymingassistant;
 
 import android.app.Fragment;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +13,22 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.a7ate9.aparvez.rhymingassistant.utils.HelperUtility;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by aparvez on 3/6/17.
  */
@@ -19,6 +37,8 @@ public class PlaceholderFragment extends Fragment {
     private EditText wordTextBox;
     private Button rhymeButton;
     private ListView rhymeWordsList;
+    private List<String> rhymingWords;
+    private String word;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +46,7 @@ public class PlaceholderFragment extends Fragment {
         View fragmentView = inflater.inflate(R.layout.fragment_main, container, false);
         return fragmentView;
     }
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         getWidgetsFromID();
@@ -34,32 +55,115 @@ public class PlaceholderFragment extends Fragment {
         rhymeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getRhymingWords();
-                Toast.makeText(getActivity(), "Hi", Toast.LENGTH_SHORT).show();
+                word = wordTextBox.getText().toString();
+                getRhymingWords(word);
+                Toast.makeText(getActivity(), word, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void getRhymingWords() {
+    private void getRhymingWords(String word) {
         if (HelperUtility.hasNetworkConnection(getActivity())) {
-            try
-            {
-                FetchTrailerTask fetchTrailerTask = new FetchTrailerTask();
-                FetchReviewTask fetchReviewTask = new FetchReviewTask();
-                fetchTrailerTask.execute(movie.getId());
-                fetchReviewTask.execute(movie.getId());
-            }
-            catch (Exception e){
-                Log.d("Exception",e.toString());
+            try {
+                FetchRhymingWords fetchRhymingWords = new FetchRhymingWords();
+                fetchRhymingWords.execute(word);
+            } catch (Exception e) {
+                Log.d("Exception", e.toString());
             }
         }
     }
 
-    public void getWidgetsFromID(){
+    public void getWidgetsFromID() {
         wordTextBox = (EditText) getActivity().findViewById(R.id.wordTextBox);
         rhymeButton = (Button) getActivity().findViewById(R.id.rhymeButton);
         rhymeWordsList = (ListView) getActivity().findViewById(R.id.rhymeWordList);
     }
 
 
+    private class FetchRhymingWords extends AsyncTask<String, Void, List<String>> {
+        private final String LOG_TAG = FetchRhymingWords.class.getSimpleName();
+
+        @Override
+        protected List<String> doInBackground(String... params) {
+            if (params.length == 0) {
+                return null;
+            }
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String rhymingWordsJsonString = null;
+
+            try {
+                final String DATA_MUSE_API_BASE_URL = "https://api.datamuse.com/words?rel_rhy=";
+                URL url = new URL(DATA_MUSE_API_BASE_URL + params[0]);
+                Log.v(LOG_TAG, url.toString());
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0)
+                    return null;
+
+                rhymingWordsJsonString = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+
+                try {
+                    return getRhymingWordsFromJson(rhymingWordsJsonString);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+        }
+
+        private List<String> getRhymingWordsFromJson(String rhymingWordsJsonString)
+                throws JSONException {
+            final String WORD = "word";
+            rhymingWords = new ArrayList<>();
+            JSONArray jsonWordsArray = new JSONArray(rhymingWordsJsonString);
+
+            for (int i = 0; i < jsonWordsArray.length(); i++) {
+                JSONObject currentJSONObject = jsonWordsArray.getJSONObject(1);
+                String rhymedWord = currentJSONObject.get(WORD).toString();
+                rhymingWords.add(rhymedWord);
+            }
+            return rhymingWords;
+        }
+
+        @Override
+        protected void onPostExecute(List<String> rhymingWords) {
+            super.onPostExecute(rhymingWords);
+            if (rhymingWords != null) {
+//                final RhymingWordAdapter rhymingWordAdapter = new RhymingWordAdapter (getActivity(),rhymingWords);
+//                rhymeWordsList.setAdapter(rhymingWordAdapter);
+            }
+        }
+
+    }
 }
---
+++ app/src/main/java/com/a7ate9/aparvez/rhymingassistant/RhymingWordAdapter.java
@@ -0,0 +1,22 @@
+package com.a7ate9.aparvez.rhymingassistant;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by aparvez on 3/6/17.
+ */
+
+public class RhymingWordAdapter {
+
+}
